dbfa84129936506201f6aa9f724c7361ab294e6f
William <messhias@gmail.com>
2018-03-09 00:55:41 -0300

    todo list aplication front development - Still need to finish it

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'React';
+import PageHeader from '../template/PageHeader';
+
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+);
---
+++ todo-app/frontend/src/todo/todo.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PageHeader from '../template/PageHeader';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
+export default class Todo extends Component {
+
+    render() {
+        return (
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm />
+                <TodoList />
+            </div>
+        );
+    }
+
+}
